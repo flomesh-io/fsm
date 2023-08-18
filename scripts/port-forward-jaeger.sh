75543d11721dff743aa235ec668fe0a7183404f7
Addo.Zhang <duwasai@gmail.com>
2023-08-18 15:18:17 +0800

    update port forwarding shell

--- scripts/port-forward-jaeger.sh
+++ scripts/port-forward-jaeger.sh
@@ -5,6 +5,6 @@ set -aueo pipefail
 
 source .env
 
-FSM_POD=$(kubectl get pods -n "$K8S_NAMESPACE" --no-headers  --selector app=jaeger | awk 'NR==1{print $1}')
+FSM_POD=$(kubectl get pods -n "$K8S_NAMESPACE" --no-headers  --selector app=fsm-jaeger | awk 'NR==1{print $1}')
 
 kubectl port-forward -n "$K8S_NAMESPACE" "$FSM_POD"  16686:16686 --address 0.0.0.0
