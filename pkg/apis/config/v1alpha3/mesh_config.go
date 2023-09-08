bcedd21293970607854519fc190f5f1eb78034a9
Lin Yang <reaver@flomesh.io>
2023-09-08 16:50:59 +0800

    build: bump pipy to 0.90.3-2 (#58)
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- pkg/apis/config/v1alpha3/mesh_config.go
+++ pkg/apis/config/v1alpha3/mesh_config.go
@@ -631,7 +631,7 @@ type MiscSpec struct {
 	// CurlImage defines the image of curl.
 	CurlImage string `json:"curlImage"`
 
-	// +kubebuilder:default="flomesh/pipy-repo:0.90.2-41"
+	// +kubebuilder:default="flomesh/pipy-repo:0.90.3-2"
 	// RepoServerImage defines the image of repo server.
 	RepoServerImage string `json:"repoServerImage"`
 }
