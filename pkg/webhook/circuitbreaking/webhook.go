6243267a0e826d36a32c361f930a88c8f4f238f1
Lin Yang <reaver@flomesh.io>
2023-11-15 09:05:37 +0800

    fix: fast fail for the wrong target reference (#116)
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- pkg/webhook/circuitbreaking/webhook.go
+++ pkg/webhook/circuitbreaking/webhook.go
@@ -212,8 +212,11 @@ func doValidation(obj interface{}) error {
 	}
 
 	errorList := validateTargetRef(policy.Spec.TargetRef)
-	errorList = append(errorList, validateConfig(policy)...)
+	if len(errorList) > 0 {
+		return utils.ErrorListToError(errorList)
+	}
 
+	errorList = append(errorList, validateConfig(policy)...)
 	if len(errorList) > 0 {
 		return utils.ErrorListToError(errorList)
 	}
