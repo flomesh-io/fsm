198212e65a60ff82bd2a0d23a6a90208e3ad3b90
Cybwan <baili@flomesh.io>
2024-08-06 20:26:44 +0800

    fix load balance endpoints registrations for connector.

--- pkg/connector/cli/flags.go
+++ pkg/connector/cli/flags.go
@@ -80,7 +80,7 @@ func init() {
 	flags.StringVar(&Cfg.FsmMeshConfigName, "fsm-config-name", "fsm-mesh-config", "Name of the FSM MeshConfig")
 	flags.StringVar(&Cfg.FsmVersion, "fsm-version", "", "Version of FSM")
 	flags.StringVar(&Cfg.TrustDomain, "trust-domain", "cluster.local", "The trust domain to use as part of the common name when requesting new certificates")
-	flags.IntVar(&Cfg.CtoKWorkers, "ctok-workers", 50, "Number of CtoK workers")
+	flags.IntVar(&Cfg.CtoKWorkers, "ctok-workers", 100, "Number of CtoK workers")
 	flags.StringVar(&Cfg.SdrProvider, "sdr-provider", "", "service discovery and registration (consul, eureka, nacos, machine, gateway)")
 	flags.StringVar(&Cfg.SdrConnector, "sdr-connector", "", "connector name")
 }
