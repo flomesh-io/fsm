600cdc5d716ba24f7a19172a1787b6d85757b27a
Lin Yang <reaver@flomesh.io>
2024-10-09 16:28:29 +0800

    feat: new Access Log format
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- charts/fsm/components/scripts/gateways/filters/http/AccessLog.js
+++ charts/fsm/components/scripts/gateways/filters/http/AccessLog.js
@@ -10,34 +10,24 @@ export default function (config) {
       var headers = $ctx.head.headers || {}
       var response = $ctx.response
       var target = $ctx.target
-      log({
-        headers: headers,
-        protocol: $ctx.head.protocol || '',
-        upstream_service_time: response.headTime,
-        upstream_local_address: target || '',
-        duration: response.tailTime - $ctx.headTime,
-        upstream_transport_failure_reason: '',
-        route_name: '',
-        downstream_local_address: inbound.localAddress,
-        user_agent: headers['user-agent'] || '',
-        response_code: response.head?.status,
-        response_flags: '',
-        start_time: $ctx.headTime,
-        method: $ctx.head.method || '',
-        request_id: $ctx.id,
-        upstream_host: target,
-        x_forward_for: headers['x-forwarded-for'] || '',
-        client_ip: inbound.remoteAddress,
-        requested_server_name: '',
-        bytes_received: $ctx.tail.headSize + $ctx.tail.bodySize,
-        bytes_sent: response.tail ? response.tail.headSize + response.tail.bodySize : 0,
-        upstream_cluster: $ctx.backendResource?.metadata?.name,
-        downstream_remote_address: inbound.remoteAddress,
-        authority: $ctx.head.authority || '',
-        path: $ctx.path,
-        response_code_details: response.head?.statusText,
-        trace_id: headers['x-b3-traceid'] || '',
-      })
+
+      // Log the request and response
+      // Format: remoteAddress (traceId) - [timestamp] "method path protocol" statusCode statusText "user-agent" responseTime requestSize responseSize "upstream" "backend"
+      log(
+        `${inbound.remoteAddress}`,
+        `(${headers['x-b3-traceid'] || ''})`,
+        `-`,
+        `[${new Date().toISOString()}]`,
+        `"${$ctx.head?.method || ''} ${$ctx.path} ${$ctx.head?.protocol || ''}"`,
+        `${response.head?.status}`,
+        `${response.head?.statusText || ''}`,
+        `"${headers['user-agent'] || ''}"`,
+        `${response.headTime - $ctx.headTime}ms`,
+        `${$ctx.tail.headSize + $ctx.tail.bodySize}`,
+        `${response.tail ? response.tail.headSize + response.tail.bodySize : 0}`,
+        `"${$ctx.backendResource?.metadata?.name || ''}"`,
+        `"${target || ''}"`
+      )
     })
   )
 }
