d000957b9d348d96d74e140bda36e8971e3a6cbc
Lin Yang <reaver@flomesh.io>
2024-10-30 08:06:39 +0800

    build(deps): bump pipy to 1.5.3
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- tests/e2e/e2e_fsm_ingress_test.go
+++ tests/e2e/e2e_fsm_ingress_test.go
@@ -243,7 +243,7 @@ func deployAppForTestingIngress() {
 					Containers: []corev1.Container{
 						{
 							Name:  "pipy",
-							Image: "flomesh/pipy:1.5.2",
+							Image: "flomesh/pipy:1.5.3",
 							Ports: []corev1.ContainerPort{
 								{
 									Name:          "pipy",
