3135b8e13cc27a3565de56a5a9d4612b52491061
Lin Yang <reaver@flomesh.io>
2025-01-07 00:04:45 +0800

    fix: enable service-lb only when running on kind
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- tests/e2e/e2e_gatewayapi_test.go
+++ tests/e2e/e2e_gatewayapi_test.go
@@ -45,37 +45,7 @@ var _ = FSMDescribe("Test traffic routing by FSM Gateway with trafficInterceptio
 	func() {
 		Context("Test traffic from client to backend service routing by FSM Gateway", func() {
 			It("allow traffic of multiple protocols through Gateway", func() {
-				// Install FSM
-				installOpts := Td.GetFSMInstallOpts()
-				installOpts.EnableIngress = false
-				installOpts.EnableGateway = true
-				installOpts.EnableServiceLB = true
-				installOpts.TrafficInterceptionMode = constants.TrafficInterceptionModePodLevel
-
-				Expect(Td.InstallFSM(installOpts)).To(Succeed())
-				Expect(Td.WaitForPodsRunningReady(Td.FsmNamespace, 3, &metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"app.kubernetes.io/instance": "fsm",
-						"app.kubernetes.io/name":     "flomesh.io",
-					},
-				})).To(Succeed())
-
-				testDeployFSMGateway()
-
-				testFSMGatewayHTTPTrafficSameNamespace()
-				testFSMGatewayHTTPTrafficCrossNamespace()
-				testFSMGatewayHTTPSTraffic()
-				testFSMGatewayTLSTerminate()
-				testFSMGatewayTLSPassthrough()
-
-				testFSMGatewayGRPCTrafficSameNamespace()
-				testFSMGatewayGRPCTrafficCrossNamespace()
-				testFSMGatewayGRPCSTraffic()
-
-				testFSMGatewayTCPTrafficSameNamespace()
-				testFSMGatewayTCPTrafficCrossNamespace()
-				testFSMGatewayUDPTrafficSameNamespace()
-				testFSMGatewayUDPTrafficCrossNamespace()
+				testGatewayAPI(constants.TrafficInterceptionModePodLevel)
 			})
 		})
 	})
@@ -89,40 +59,44 @@ var _ = FSMDescribe("Test traffic routing by FSM Gateway with trafficInterceptio
 	func() {
 		Context("Test traffic from client to backend service routing by FSM Gateway", func() {
 			It("allow traffic of multiple protocols through Gateway", func() {
-				// Install FSM
-				installOpts := Td.GetFSMInstallOpts()
-				installOpts.EnableIngress = false
-				installOpts.EnableGateway = true
-				installOpts.EnableServiceLB = true
-				installOpts.TrafficInterceptionMode = constants.TrafficInterceptionModeNodeLevel
-
-				Expect(Td.InstallFSM(installOpts)).To(Succeed())
-				Expect(Td.WaitForPodsRunningReady(Td.FsmNamespace, 3, &metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"app.kubernetes.io/instance": "fsm",
-						"app.kubernetes.io/name":     "flomesh.io",
-					},
-				})).To(Succeed())
-
-				testDeployFSMGateway()
-
-				testFSMGatewayHTTPTrafficSameNamespace()
-				testFSMGatewayHTTPTrafficCrossNamespace()
-				testFSMGatewayHTTPSTraffic()
-				testFSMGatewayTLSTerminate()
-				testFSMGatewayTLSPassthrough()
-
-				testFSMGatewayGRPCTrafficSameNamespace()
-				testFSMGatewayGRPCTrafficCrossNamespace()
-				testFSMGatewayGRPCSTraffic()
-
-				testFSMGatewayTCPTrafficSameNamespace()
-				testFSMGatewayTCPTrafficCrossNamespace()
-				testFSMGatewayUDPTrafficSameNamespace()
-				testFSMGatewayUDPTrafficCrossNamespace()
+				testGatewayAPI(constants.TrafficInterceptionModeNodeLevel)
 			})
 		})
 	})
+
+func testGatewayAPI(trafficInterceptionMode string) {
+	// Install FSM
+	installOpts := Td.GetFSMInstallOpts()
+	installOpts.EnableIngress = false
+	installOpts.EnableGateway = true
+	installOpts.EnableServiceLB = Td.InstType == KindCluster
+	installOpts.TrafficInterceptionMode = trafficInterceptionMode
+
+	Expect(Td.InstallFSM(installOpts)).To(Succeed())
+	Expect(Td.WaitForPodsRunningReady(Td.FsmNamespace, 3, &metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			"app.kubernetes.io/instance": "fsm",
+			"app.kubernetes.io/name":     "flomesh.io",
+		},
+	})).To(Succeed())
+
+	testDeployFSMGateway()
+
+	testFSMGatewayHTTPTrafficSameNamespace()
+	testFSMGatewayHTTPTrafficCrossNamespace()
+	testFSMGatewayHTTPSTraffic()
+	testFSMGatewayTLSTerminate()
+	testFSMGatewayTLSPassthrough()
+
+	testFSMGatewayGRPCTrafficSameNamespace()
+	testFSMGatewayGRPCTrafficCrossNamespace()
+	testFSMGatewayGRPCSTraffic()
+
+	testFSMGatewayTCPTrafficSameNamespace()
+	testFSMGatewayTCPTrafficCrossNamespace()
+	testFSMGatewayUDPTrafficSameNamespace()
+	testFSMGatewayUDPTrafficCrossNamespace()
+}
 
 func testDeployFSMGateway() {
 	// Create namespaces
