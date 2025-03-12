cb6f525c4c65ba93554f2d944e86bdab6fee7421
Lin Yang <reaver@flomesh.io>
2025-03-13 01:29:24 +0800

    test: use github.com/go-sql-driver/mysql as mysql driver
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- demo/cmd/database/mysql.go
+++ demo/cmd/database/mysql.go
@@ -5,8 +5,9 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	gomysql "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -21,9 +22,16 @@ const (
 
 // GetMySQLConnection returns a MySQL connection using default configuration
 func GetMySQLConnection() (*gorm.DB, error) {
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&timeout=20s", dbuser, dbpass, "mysql.bookwarehouse", dbport, dbname)
+	cfg := gomysql.NewConfig()
+	cfg.User = dbuser
+	cfg.Passwd = dbpass
+	cfg.Net = "tcp"
+	cfg.Addr = fmt.Sprintf("%s:%d", "mysql.bookwarehouse", dbport)
+	cfg.DBName = dbname
+	cfg.Timeout = 20 * time.Second
+	cfg.Params = map[string]string{"charset": "utf8"}
 
-	if sqlDB, err := sql.Open("mysql", connStr); err != nil {
+	if sqlDB, err := sql.Open("mysql", cfg.FormatDSN()); err != nil {
 		return nil, err
 	} else {
 		return gorm.Open(mysql.New(mysql.Config{
@@ -31,6 +39,7 @@ func GetMySQLConnection() (*gorm.DB, error) {
 		}), &gorm.Config{})
 	}
 
+	//connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&timeout=20s", dbuser, dbpass, "mysql.bookwarehouse", dbport, dbname)
 	//db, err := gorm.Open(mysql..Open(connStr), &gorm.Config{})
 	//
 	//return db, err
