6d219f6baf995b2fa1c6a2edf536ae3dca84ade2
Kevein Liu <kevein@flomesh.cn>
2026-05-25 21:16:36 +0800

    Revert "feat(connector): store raw Nacos metadata as base64 for annotation mapping"
    This reverts commit b5a82853b9176525d48aaec9e5c127a3eb761eaa.

--- pkg/connector/discovery.go
+++ pkg/connector/discovery.go
@@ -175,16 +175,8 @@ func (as *AgentService) FromNacos(ins *nacos.Instance) {
 		for k, v := range ins.Metadata {
 			as.Meta[k] = v
 		}
-		dubboMeta := make(map[string]string)
-		for k, v := range ins.Metadata {
-			if strings.HasPrefix(k, "dubbo.") {
-				dubboMeta[k] = v
-			}
-		}
-		if len(dubboMeta) > 0 {
-			if metadataBytes, err := json.Marshal(dubboMeta); err == nil {
-				as.Meta["_raw.dubbo.metadata"] = base64.StdEncoding.EncodeToString(metadataBytes)
-			}
+		if metadataBytes, err := json.Marshal(ins.Metadata); err == nil {
+			as.Meta["_raw.nacos.metadata"] = base64.StdEncoding.EncodeToString(metadataBytes)
 		}
 	}
 }
